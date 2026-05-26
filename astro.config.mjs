c162c867311aa88982a686fecbe075ae35d1ffa8
qcharlieshi <charlies@live.com>
2026-05-26 10:28:52 -0400

    fix: issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
-  output: "hybrid",
+  output: 'static',
   integrations: [tailwind()],
-  adapter: cloudflare()
 });
